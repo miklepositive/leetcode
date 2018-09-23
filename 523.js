0f565a1ae531009f065325e4d117e699cc540e75
Mikle Petrusenko <miklepositive@gmail.com>
2018-09-23 13:30:13 +0300

    refactor after Vitaliy K. review

--- 523.js
+++ 523.js
@@ -12,6 +12,7 @@ Example 1:
 
 Input: [1,1,2,3,3,4,4,8,8]
 Output: 2
+
 Example 2:
 
 Input: [3,3,7,7,10,11,11]
@@ -21,10 +22,7 @@ Note: Your solution should run in O(log n) time and O(1) space.
 var singleNonDuplicate = function(nums) {
   var single = 777;
   var flag = false;
-  for (var i = 0; i < nums.length; i++) {
-    if (flag) {
-      break;
-    }
+  for (var i = 0; i < nums.length && !flag; i++) {
     for (var j = 0; j < nums.length; j++) {
       if (i == j) {
         continue;
@@ -40,7 +38,10 @@ var singleNonDuplicate = function(nums) {
   return single;
 };
 
+// for testing
 var array = [2, 2, 1, 3, 3, 10, 10, 2, 1, 2, 13];
 var array1 = [1, 2, 2, 3, 3, 6, 6];
+var array2 = [2, 2, 7, 3, 3, 6, 6];
 console.log(singleNonDuplicate(array));
 console.log(singleNonDuplicate(array1));
+console.log(singleNonDuplicate(array2));
---
+++ README.md
@@ -0,0 +1,4 @@
+# leetcode
+ Tasks using JS
+
+ Have just created this repo for myself. Here will be solved tasks from the leetcode.com
